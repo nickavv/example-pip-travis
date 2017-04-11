03a44aef45cbbd76e61c700b08d0f2fb4ecd47b6
nickavv <talkto@nicholasvv.com>
2017-04-11 15:37:23 -0400

    fix setup py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=["requests", "numpy"],
     entry_points={
       'console_scripts': [
-          'example-pip-travis = example-pip-travis.__main__:main'
+          'example-pip-travis=example-pip-travis.__main__:main'
       ]
     }
 )
